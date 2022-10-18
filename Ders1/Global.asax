2927fafa5eb2e5626cf0757d772e1736fe56e21e
mehme <mehme@MEHMET>
2022-10-18 15:40:53 +0300

    Add project files.

---
+++ Ders1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ders1.MvcApplication" Language="C#" %>
---
+++ EntityLayer/Concrate/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Category
+    {
+        public int CatergoryID { get; set; }
+        public string CatergoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
